c4edd006734597eb49886ffd28273082f486a6cb
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-10-10 10:20:55 +0000

    Zrobiono zadania 1-4

---
+++ KartaPracy2.py
@@ -0,0 +1,28 @@
+# # Zad 1
+# n = int(input())
+# if n % 3 == 0:
+#   print ("Tak")
+# else:
+#   print ("Nie")
+
+# Zad 2 
+# n = int(input())
+# if n >= 100 and n <1000 and n % 17 == 0:
+#   print("Tak")
+# else:
+#   print("Nie")
+
+# Zad 3
+# wiek = int(input())
+# if wiek >= 18:
+#   print("Tak")
+# else:
+#   print("Nie")
+
+# Zad 4
+LIMIT = 20
+waga = int(input())
+if waga <= LIMIT:
+  print("Jedz ze spokojem")
+else:
+  print("Lepiej nie jedz")
