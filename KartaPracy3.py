19ccf94ba542604e3826713be3b9095cfeecb3a7
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-10-17 10:20:28 +0000

    Zrobiono zadania 1,2,3

---
+++ KartaPracy3.py
@@ -0,0 +1,33 @@
+# PRE
+
+1# pętla liczb dwucyfrowych od 10 do 21
+2# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+3# pętla liczb dwucyfrowych malejąco (step ujemny)
+4# pętla liczb dwucyfrowych malejąco (step dodatni)
+5# pętla liczb 3-cyfrowych podzielnych przez 20
+
+
+# print(list (range (2, 8, 1)))
+# print(list (range (1, 8)))
+# print(list (range (8, 1, -1)))
+# print(list (range (10)))
+# for i in range(3, 10):
+#   print(i)
+
+# Zad 1
+# for i in range(10, 22, 1):
+#   print(i)
+
+# Zad 2
+# for i in range(15, 32, 2):
+#   print(i)
+
+# Zad 3
+# for i in range(99, 9, -1):
+#   print(i)
+
+# Zad 5
+for i in range()
+  
+
+              
