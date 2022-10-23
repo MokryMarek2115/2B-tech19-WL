ac36ee9e3535c0f37785e3c39b63c3e700c95da4
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-10-23 08:43:53 +0000

    nie dokonczone

---
+++ KartaPracy3b.py
@@ -0,0 +1,34 @@
+# Zad 1
+# for i in range(1,31,1):
+#  print(i)  
+  
+
+# Zad 2
+# n=int(input())
+# for n in range(n):
+#   print((n*2+1)**2)
+
+
+# Zad 3
+# for i in range(1, 379, 1):
+#   if i % 4 == 0:
+#     print(i, end=" ")
+
+# Zad 4
+# for i in range(100, 1000, 1):
+#   if i % 5 == 0 or i % 6 == 0 or i % 11 == 0:
+#     print(i, end=" ")
+
+
+# Zad 5
+n=int(input())
+for n in range(n):
+  print
+
+
+# Zad 6
+n=int(input())
+n % 2 == 0
+k="suma"
+for n in range(n):
+  print(n+n)
