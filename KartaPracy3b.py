92ad712adc3784106f2b287041a192f0160f395e
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-11-01 21:13:41 +0000

    Ukończono kartę pracy

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -10,8 +10,8 @@
 
 
 # Zad 3
-# for i in range(1, 379, 1):
-#   if i % 4 == 0:
+# for i in range(1000,10000 , 1):
+#   if i % 379 == 0:
 #     print(i, end=" ")
 
 # Zad 4
@@ -21,14 +21,52 @@
 
 
 # Zad 5
-n=int(input())
-for n in range(n):
-  print
+# s = 0
+# n = int(input())
+# for i in range(n):
+#   k=int(input())
+#   s = s + k
+# print(s)
+
 
 
 # Zad 6
-n=int(input())
-n % 2 == 0
-k="suma"
-for n in range(n):
-  print(n+n)
+# s = 0
+# k = int(input())
+# for i in range(k):
+#   if i % 2 == 0: 
+#     s = s + i
+#     print(s)
+
+# Zad 7
+# s = 0
+# m = int(input())
+# for i in range(m):
+#   if i % 2 == 1:
+#     s = s + i
+#     print(s)
+
+# Zad 8
+# W = int(input())
+# L = int(input())
+# c = 0
+# suma = W
+# for i in range(0, L * 12):
+#   c = suma * 0.06 * (1/12)
+#   suma += c
+# print(round(suma,2))
+
+# Zad 9
+# n = int(input())
+# s = 0
+# for i in range(21 , n+1, 100):
+#   s = s + i
+# print(s)
+
+# Zad 10
+# import math
+# for i in range(1, 1000):
+#   if i % 10 == math.sqrt(i):
+#     print(i)
+#   if i % 100 == math.sqrt(i):
+#     print(i)
---
+++ Temp.py
@@ -0,0 +1,36 @@
+# Zad 1
+# for i in range(1,31,1):
+#  print(i)  
+  
+
+# Zad 2
+# n=int(input())
+# for n in range(n):
+#   print((n*2+1)**2)
+
+
+# Zad 3
+# for i in range(1000,10000 , 1):
+#   if i % 4 == 0:
+#     print(i, end=" ")
+
+# Zad 4
+# for i in range(100, 1000, 1):
+#   if i % 5 == 0 or i % 6 == 0 or i % 11 == 0:
+#     print(i, end=" ")
+
+
+# Zad 5
+# s = 0
+# n = int(input())
+# for i in range(n):
+#   k=int(input())
+#   s = s + k
+# print(s)
+
+# Zad 6
+# s = 0
+# k = int(input())
+# for i in range(k):
+
+  
