2be638f74a4232588f0969b0388b52b2172e90ed
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-11-02 13:16:39 +0000

    zrobiono zadanie 1

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -77,14 +77,22 @@
   
   
 # Zad 6
-
+# 1 2 3 5 8 13 
+# n=int(input())
+# a = 0
+# b = 1
+# for i in range(n):
+#   a, b =b, b + a
+# print(b)
+  
+  
 
 
 
 
 
 # Napisz pętle sumującą liczby dwucyfrowe parzyste
-suma = 0
-for i in range(10, 100, 2):
-  suma = (suma + i)
-print(suma)
+# suma = 0
+# for i in range(10, 100, 2):
+#   suma = (suma + i)
+# print(suma)
---
+++ KartaPracy3a.py
@@ -0,0 +1,7 @@
+# Zad 1
+# n=int(input())
+# for i in range(n):
+#   print("*-|", end=" ")
+
+# n=int(input())
+# print("*-|"*n)
