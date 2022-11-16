2db8c54707747db09c77645886bf230d0b16bc14
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-11-16 12:51:47 +0000

    Zadania od 1-3

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -5,3 +5,22 @@
 
 # n=int(input())
 # print("*-|"*n)
+
+
+# Zad 2
+# n=int(input())
+# for i in range(1, n+1):
+#   print("*"*i, end=" ")
+#   if i % 2 == 0:
+#     print("--", end=" ")
+#   else:
+#     print("||", end=" ")
+
+# Zad 3
+n=int(input())
+for i in range(1, n+1):
+  print("*", end=" ")
+  if i % 2 == 0:
+    print("-"*i, end=" ")
+  else:
+    print("|"*i, end=" ")
---
+++ Sprawdzian.py
@@ -0,0 +1,18 @@
+# 1
+
+a=int(input())
+b=int(input())
+c=int(input())
+for i in range(2,9):
+  if a - b == i or b - a == i or b - c == i or c - b == i:
+    print("dobry kod")
+  elif a - b == 1 or b - a == 1 or b - c == 1 or c - b == 1:
+    print("niedopracowany kod")
+
+
+
+
+# 2
+# n=int(input())
+
+
