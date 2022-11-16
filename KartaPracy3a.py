2f1836297691a2327c201db3816c0f079680a7d8
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-11-16 13:00:24 +0000

    DODATKOWE zadanie

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -17,10 +17,22 @@
 #     print("||", end=" ")
 
 # Zad 3
+# n=int(input())
+# for i in range(1, n+1):
+#   print("*", end=" ")
+#   if i % 2 == 0:
+#     print("-"*i, end=" ")
+#   else:
+#     print("|"*i, end=" ")
+
+
+# Zad 7 DODATEK
 n=int(input())
-for i in range(1, n+1):
-  print("*", end=" ")
-  if i % 2 == 0:
-    print("-"*i, end=" ")
-  else:
-    print("|"*i, end=" ")
+for i in range(n):
+  for j in range(n):
+    # print(f"({i}, {j})", end=" ")
+    if i == 0 or j == 0 or j==n-1 or i==n-1 or (i==n//2 and j==n//2):
+      print("*", end=" ")
+    else:
+      print("-", end=" ")
+  print()
