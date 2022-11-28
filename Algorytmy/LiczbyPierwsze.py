f9545a685e29a7422e6ad1c2dd12e28ec3793c6a
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-11-28 11:19:52 +0000

    Euklides

---
+++ Algorytmy/LiczbyPierwsze.py
@@ -0,0 +1,47 @@
+# Zad 1 Sprawdzanie czy liczba jest pierwsza
+#liczba pierwsza to taka liczba ktora sie dzieli tylko przez 1 i sama siebie, np. 2,3,5,7,11,13,17,19,23.....
+#dzielniki własciwe - dzielniki liczby poza 1 i nia sama
+
+# n=int(input())
+# for i in range(2, n):
+#   if n % i == 0:
+#     print("Liczba nie jest pierwsza")
+#     exit(0)
+# print("Liczba jest pierwsza")
+
+
+# 2. Generowanie liczb pierwszych(w przedziale [p, q])
+
+# p=int(input("Podaj do ilu mam szukac liczb pierwszych"))
+# q=int(input("Podaj do ilu mam szukac liczb pierwszych"))
+
+# for i in range(p, q+1):
+#   flaga = True;
+#   for j in range(2, i):
+#     if i % j == 0:
+#       flaga = False
+#       break
+#   if flaga == True:
+    # print(i, end=" ")
+
+# 3. Generowanie liczb pierwszych(pierwsze n liczb pierwszych)
+
+# p=int(input("Podaj ile (od początku) liczb pierwszych chcesz"))
+
+# i = 2
+# licznik = 0
+
+# while 1:
+#   flaga = True;
+#   for j in range(2, int(i**0.5)+1):
+#     if i % j == 0:
+#       flaga = False
+#       break
+#   if flaga == True:
+#     print(i, end=" ")
+#     licznik += 1
+#   if licznik == p:
+#     break
+#   else:
+#     i = i + 1
+
---
+++ challenge.py
@@ -0,0 +1,25 @@
+# 1 
+# suma cyfr od 21 do 32
+
+# s=0
+# for i in range(21,33):
+#   s=s+i
+# print(s)
+  
+
+
+
+
+
+
+#2
+# sprawdz czy liczba jest 3 cyfrowa i nie parzysta
+
+n=int(input())
+if n>99 and n<1000:
+  if n%2!=0:
+    print("jest git")
+  else:
+    print("nie")
+else:
+  print("Nie")
