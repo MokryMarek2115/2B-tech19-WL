f6d21381a1409edb16ebf0a6f6cec98158ef2d40
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-12-14 13:14:50 +0000

    Zrobiono zadanie 4 i 5 częściowo

--- KartaPracy6.py
+++ KartaPracy6.py
@@ -20,10 +20,39 @@
 #     s = s + i
 # print(s)
 
+# Zad 3
+# for i in range(99,9,-1):
+#   if i % 7==0:
+#     wielok = i
+#     break
+
+# ilosc=0
+# for i in range(1000,10000):
+#   if i % wielok==0:
+#     ilosc=ilosc + 1
+
+# print(ilosc)
+
+
+
 # Zad 4
+# ilosc=0
+# for i in range(10,100):
+#   cd = i // 10
+#   cj = i % 10
+#   if cd >= 2*cj:
+#     ilosc += 1
+# print(ilosc)
+
+
+# Zad 5
 ilosc=0
+s=0
+
 for i in range(10,100):
-  cd = i // 10
+  cs = i // 100
+  cd = (i % 100) // 10
+  #cd = (i // 10) % 10
   cj = i % 10
   if cd >= 2*cj:
     ilosc += 1
