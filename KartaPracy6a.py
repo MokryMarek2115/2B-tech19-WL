cc9c7f70cc1560a4f9d0eb6536703f9b7727e1de
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2022-12-19 11:20:02 +0000

    zadanie 1

---
+++ KartaPracy6a.py
@@ -0,0 +1,29 @@
+# Zad 1
+
+# ------------------------------
+# n % 10 
+# n // 10
+
+# 357 % 10 = 7
+# 357 // 10 = 35
+# 
+# cyfra = 357 % 10
+# liczba = 357 // 10
+
+# ------------------------------
+
+# n=int(input())
+
+# suma = 0 
+# while n > 0:
+#   cyfra = n % 10
+#   suma = suma + cyfra 
+#   n = n // 10
+# print(suma)
+
+
+# Zad 2
+n = int(input())
+for i in range():
+  if n % 1        
+  
