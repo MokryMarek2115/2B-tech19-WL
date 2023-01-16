97ad3ed34ff67939754929fead4e0ed0b3b73cdc
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2023-01-16 11:16:03 +0000

    Początek PseudoKod

---
+++ PseudoKod.py
@@ -0,0 +1,73 @@
+# --EUKLIDES ODEJMOWANIE
+
+# a = int(input())
+# b = int(input())
+# while a != b:
+#     if a > b: a = a - b
+#     if b > a: b = b - a
+# print(a)
+
+# --PSEUDOKOD 1
+
+# dopóki a =/ b wykonuj  (=/ oznacza różne jak w zeszycie)
+#     jeżeli a > b
+#         a = a - b
+#     jeżeli b > a
+#         b = b - a
+# wypisz a
+
+# --EUKLIDES MODULO
+
+# a = int(input())
+# b = int(input())
+# while b > 0:
+#     a, b = b, a % b
+# print(a)
+
+# ---PSEUDOKOD
+
+# dopóki b > 0 wykonuj
+#     temp = a
+#     a = b
+#     b = temp mod b
+# wypisz a
+
+
+
+
+# --PSEUDOKOD 2
+
+# -1 Suma liczb dwucyfrowcyh
+
+# suma = 0
+# for i in range(10,100):
+#   suma = suma + i
+# print(suma)
+
+
+# -Suma liczb dwucyfrowych(PseudoKod)
+
+#suma = 0
+# dla każdego i = 11,12,13,14,...,99 wykonuj
+#   suma = suma + 1
+# wypisz suma
+
+
+
+# -2 Napisz program który obliczy sumę k liczb wpisanych przez usera
+
+# k = int(input())
+# suma = 0
+# for i in range(k):
+#   b = int(input())
+#   suma = suma + b
+# print(suma)
+
+# -Napisz program który obliczy sumę k liczb wpisanych przez usera(PseudoKod)
+
+# wpisz k 
+# suma = 0
+# dla każdego  i = 1,2,...,k wykonuj
+#   czytaj x
+#   suma = suma + x
+# wypisz suma
--- challenge.py
+++ challenge.py
@@ -15,11 +15,11 @@
 #2
 # sprawdz czy liczba jest 3 cyfrowa i nie parzysta
 
-n=int(input())
-if n>99 and n<1000:
-  if n%2!=0:
-    print("jest git")
-  else:
-    print("nie")
-else:
-  print("Nie")
+# n=int(input())
+# if n>99 and n<1000:
+#   if n%2!=0:
+#     print("jest git")
+#   else:
+#     print("nie")
+# else:
+#   print("Nie")
