3a8d086beed3f030e32904fcceea76653d3cbd7d
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2023-03-01 13:14:51 +0000

    Napisy początek

---
+++ Napisy/Nap.1.py
@@ -0,0 +1,21 @@
+# s = input()
+# L = list(s)
+# print(s, L)
+# L.sort()
+# s = "".join(L)
+# print(s, L)
+
+
+
+# Sprawdź czy wyraz jest palindromem
+
+# s=input()
+# L=list(s)
+# R=list(s)
+# print(s, L)
+# L.reverse()
+# if L == R:
+#   print("TAK")
+# else:
+#   print("NIE")
+
