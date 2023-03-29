2c24302e99a0dceb00c32d0c6dc9efbcd3a8dc2e
1B12LW <114583311+MokryMarek2115@users.noreply.github.com>
2023-03-29 12:15:49 +0000

    Wiecej

---
+++ Napisy/Nap.2.py
@@ -0,0 +1,25 @@
+# palindrom za pomocą tablic
+
+
+
+# anagram za pomocą tablicy
+
+# Sposób I (za pomocą funkcji)
+
+# a = input()
+# b = input()
+  
+# A=list(a)
+# B=list(b)
+
+# A.sort()
+# B.sort()
+
+# b = " ".join(A)
+# a = " ".join(B)
+
+# print(a, b)
+# if a == b:
+#   print("Tak")
+# else:
+#   print("Nie")
