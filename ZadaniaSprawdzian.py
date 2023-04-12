9cf8a318278caeaf804a44b83acefa522872509d
MokryMarek2115 <114583311+MokryMarek2115@users.noreply.github.com>
2023-04-12 12:11:10 +0000

    sprawdzian

---
+++ ZadaniaSprawdzian.py
@@ -0,0 +1,47 @@
+#GR 1
+#Zad 1 
+# s=input()
+# for i in range(len(s)):
+#   if s.count(s[i]) > 1:
+#     dubel = s[i]
+
+# pocz = s.index(dubel)
+# kon = s.index(dubel,s.index(dubel)+1)
+# print(s[pocz+1:kon])
+
+
+  
+  
+  
+
+
+
+# Zad2
+# a=input()
+# l=list(a)
+# l.reverse()
+# a = "".join(l)
+# print(a)
+
+
+
+
+
+
+
+#GR 2
+#Zad 1
+# a = input()
+# b = input()
+# if a in b:
+
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad 2
+# s=input()
+# if s.count("a") >= 2 and s.count("b") >= 1 and s.count("z") >= 1:
+#   print("Tak bedzie baza")
+# else:
+#   print("Ni ma bazy")
